eb56c59b48fcdeeea79920db585cdd6bc8cc3284
AnmolDuaAI <anmolduaai@gmail.com>
2022-08-15 13:20:29 +0530

    Added nerf code

--- Nerfs/original_nerf_config.py
+++ Nerfs/original_nerf_config.py
@@ -1,4 +1,8 @@
 
+basedir = 
+expname = 
+device = 
+
 multires = 10 # log 2 of max freq for positional encoding (3D Locations)
 multires_views = 4 # log 2 of max freq for positional encoding (2D direction)
 i_embed = 0 # 0 - for positional encoding, -1 - None
@@ -9,3 +13,16 @@ net_depth = 8 # layers in network
 net_width = 256 # channels per layer
 net_depthfine = 
 net_widthfine = 
+
+ft_path = 
+no_reload =
+
+perturb = 
+N_samples = 
+white_bkgd = 
+raw_noise_std = 
+
+dataset_type = 
+no_ndc = 
+lindisp = 
+llffhold = 
